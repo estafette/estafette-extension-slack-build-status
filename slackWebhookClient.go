7df08dd57e58464a2af09b5638fcc3d7e0fcd531
Jorrit Salverda <jsalverda@travix.com>
2018-11-13 18:24:08 +0100

    don't link title

--- slackWebhookClient.go
+++ slackWebhookClient.go
@@ -44,11 +44,10 @@ func (sc *slackWebhookClientImpl) SendMessage(target, title, message, status, li
 		Username: "Estafette CI",
 		Attachments: []SlackMessageAttachment{
 			SlackMessageAttachment{
-				Fallback:  message,
-				Title:     title,
-				TitleLink: link,
-				Text:      message,
-				Color:     color,
+				Fallback: message,
+				Title:    title,
+				Text:     message,
+				Color:    color,
 				MarkdownIn: []string{
 					"text",
 				},
